44079eb7262ee9e49c8c0e4e878d8ac3f9df56b5
dadada44 <07top.cz@ gmail.com>
2024-04-09 14:25:45 +0200

    header done

--- script/dist/script.dev.js
+++ script/dist/script.dev.js
@@ -1,16 +1,29 @@
 "use strict";
 
-var hamburger = document.querySelector(".hamburger");
+var hamburger = document.querySelector(".wrapper-hamburger");
 var nav = document.querySelector("nav");
 var state = false;
 hamburger.addEventListener("click", function () {
   if (state == true) {
     hamburger.classList.remove("is-active");
-    nav.classList.remove("is-active");
+    nav.classList.remove("is-open");
     state = false;
   } else {
     hamburger.classList.add("is-active");
-    nav.classList.add("is-active");
+    nav.classList.add("is-open");
     state = true;
   }
 });
+
+function ControlWidth() {
+  var sirka = window.innerWidth;
+  {
+    hamburger.classList.remove("is-active"); //Odebere classu s hamburgeru .is-active
+
+    nav.classList.remove("is-open"); //Odebere classu s navigace
+
+    state = false; //nastavíme zpět na defaultní hodnotu state pro kontrolu pro hamburger
+  }
+}
+
+window.addEventListener('resize', ControlWidth);
--- script/script.js
+++ script/script.js
@@ -1,4 +1,4 @@
-const hamburger = document.querySelector(".hamburger");
+const hamburger = document.querySelector(".wrapper-hamburger");
 const nav = document.querySelector("nav");
  
 let state = false;
@@ -7,11 +7,24 @@ hamburger.addEventListener("click", () => {
  
     if (state == true) {
         hamburger.classList.remove("is-active");
-        nav.classList.remove("is-active");
+        nav.classList.remove("is-open");
         state = false;
     } else {
         hamburger.classList.add("is-active");
-        nav.classList.add("is-active");
+        nav.classList.add("is-open");
         state = true;
     }
 })
+
+
+function ControlWidth() {
+    const sirka = window.innerWidth;
+    {
+        hamburger.classList.remove("is-active"); //Odebere classu s hamburgeru .is-active
+        nav.classList.remove("is-open"); //Odebere classu s navigace
+        state = false; //nastavíme zpět na defaultní hodnotu state pro kontrolu pro hamburger
+    }
+
+}
+
+window.addEventListener('resize', ControlWidth);
